f2f91470a6234283615290455234ec42da486e5c
Zakaria Yaris <info@zakariya.me>
2023-05-31 12:46:45 +0100

    add post route model

--- src/model/entries.js
+++ src/model/entries.js
@@ -1,5 +1,6 @@
-const db = require("../database/db.js");
+const db = require('../database/db.js');
 
+// get entry from db
 const select_entries = db.prepare(/*sql*/ `
   SELECT content, user_id, posted_at FROM entries
   ORDER BY posted_at DESC
@@ -7,6 +8,19 @@ const select_entries = db.prepare(/*sql*/ `
 
 function listEntries() {
   return select_entries.all();
+}
+
+const db = require('../database/db');
+
+// insert dairy entry into the db
+const create_enrty = db.prepare(/*sql*/ `
+    INSERT INTO entries (content, user_id)
+    VALUES ($content, $user_id)
+    RETURNING entries.id
+`);
+
+const createEntry = (content, user_id) => {
+  return create_enrty.get({ content, user_id });
 };
 
-module.exports = { listEntries };
+module.exports = { listEntries, createEntry };
--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const home = require("./routes/home.js");
-const signup = require("./routes/sign-up.js");
-const login = require("./routes/log-in.js");
-const logout = require("./routes/log-out.js");
-const entries = require("./routes/entries.js");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const home = require('./routes/home.js');
+const signup = require('./routes/sign-up.js');
+const login = require('./routes/log-in.js');
+const logout = require('./routes/log-out.js');
+const entries = require('./routes/entries.js');
 
 const body = express.urlencoded({ extended: false });
 const cookies = cookieParser(process.env.COOKIE_SECRET);
 
 const server = express();
 
-server.get("/", (req, res) => {
-    res.send(`<h1>Hello World</h1>`);
+server.get('/', (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
 });
 
 server.use(cookies);
@@ -22,7 +22,7 @@ server.use(cookies);
 //server.get("/log-in", login.get);
 //server.post("/log-in", body, login.post);
 //server.post("/log-out", logout.post);
-server.get("/entries/:user_id", entries.get);
+server.get('/entries/:user_id', entries.get);
 //server.post("/entries/:user_id", body, entries.post);
 
 module.exports = server;
