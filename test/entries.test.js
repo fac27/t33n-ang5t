810c2337606e2f2cd060daba30a1de678acd153f
FomasTreeman <tom@team-freeman.com>
2023-06-01 11:26:58 +0100

    initial entries test

--- test/entries.test.js
+++ test/entries.test.js
@@ -8,10 +8,26 @@ const {
   } = require("./helpers.js");
 
 // test that entries.js can fetch entries and then display them correctly
-test('should fetch entries and display them correctly', () => {
+test('should fetch entries and display them correctly', async () => {
     reset()
-    assert.strictEqual()
+    
+    const user_id = createUser('dummy', 'password')
+    const entry = createEntry(entry)
+    getEntries(user_id)
+
+    const {body} = await request(`/entries/${user_id}`)
+
+    assert.match(body, `/${entry}/`)
 })
 
 
 // test post request redirects to the correct entries page 
+test('should redirect to users entry page', async () => {
+    const {headersWithoutAuth} = await request(`/entries/1`)
+    assert.equal(headersWithoutAuth.status, 300) // unauthorized redirect
+    
+    const user_id = createUser('dummy', 'password')
+
+    const {headersWithAuth} = await request(`/entries/${user_id}`)
+    assert.equal(headersWithAuth.status, 200) // authorized redirect
+})
