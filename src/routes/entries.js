03ef746260bd7c1ee410ba917aeebdbf133795af
FomasTreeman <tom@team-freeman.com>
2023-06-01 15:13:12 +0100

    getEntries rather than listEntries

--- src/routes/entries.js
+++ src/routes/entries.js
@@ -1,11 +1,11 @@
-const { listEntries, createEntry } = require('../model/entries.js');
+const { getEntries, createEntry } = require('../model/entries.js');
 const { layout } = require('../templates/layout.js');
 const { entriesPage } = require('../templates/user-page.js');
 
 module.exports = { get, post };
 
 function get(req, res) {
-  const entries = listEntries();
+  const entries = getEntries();
   const user_id = req.session.user_id; //fix when authentication is there
   const user_name = getUser(user_id).username;
   const body = entriesPage(entries, user_id, user_name);
