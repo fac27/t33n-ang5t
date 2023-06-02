03f0588a95b775a8718ed8f6e08734624623f3ec
Alphonso <114600712+camelPhonso@users.noreply.github.com>
2023-06-02 11:48:12 +0100

    added sanitisation to entry.js

---
+++ eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'kentcdodds'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {},
+};
