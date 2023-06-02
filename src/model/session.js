7ff104ecc857f470ba97bb7ddb275ab8b806e9be
Zakaria Yaris <info@zakariya.me>
2023-06-02 10:17:26 +0100

    deployment setup branch; not working

--- src/model/session.js
+++ src/model/session.js
@@ -5,23 +5,22 @@ module.exports = { createSession, getSession, deleteSession };
 
 const select_session = db.prepare(/*sql*/ `
   SELECT *
-  FROM sessions 
+  FROM sessions
   WHERE id = ?
-  `)
+  `);
 
 function getSession(sid) {
   return select_session.get(sid);
 }
 
 const delete_session = db.prepare(/*sql*/ `
-  DELETE FROM sessions 
+  DELETE FROM sessions
   WHERE id = ?
-  `)
+  `);
 
 function deleteSession(sid) {
   return delete_session.run(sid);
 }
-
 
 const insert_session = db.prepare(/*sql*/ `
   INSERT INTO sessions (
@@ -31,11 +30,11 @@ const insert_session = db.prepare(/*sql*/ `
   ) VALUES (
     ?,
     ?,
-    DATE('now' + '7 days')
+    DATE('now', ('+7 days'))
   ) RETURNING id
 `);
 
-function createSession( user_id) {
+function createSession(user_id) {
   const id = crypto.randomBytes(18).toString('base64');
   return insert_session.get(id, user_id);
 }
